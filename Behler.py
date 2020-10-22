cd36a2adcf726dfe20053a90b63a05c9a4047200
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-10-22 13:35:17 -0600

    Create Behler.py

---
+++ Behler.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import numpy as np
+import plotly.graph_objs as go
+
+
+
+st.header("Matplotlib chart in Plotly")
+
+import matplotlib.pyplot as plt
+
+
+
+f = plt.figure()
+arr = np.random.normal(1, 1, size=100)
+plt.hist(arr, bins=20)
+
+st.plotly_chart(f)
+
