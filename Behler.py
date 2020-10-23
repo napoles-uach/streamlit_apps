579f19d7ef505bcac018fa20ff3007067d8000fb
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-10-23 14:18:19 -0600

    Update Behler.py

--- Behler.py
+++ Behler.py
@@ -16,17 +16,11 @@ st.write('The main idea is to represent the total energy $E$ of the system as a 
 
 st.write('$$E=\sum_i E_i$$')
 
-st.write('The general structure of this new network topology is shown schematically for a system consisting of three atoms.')
+st.write('The general structure of this new network topology is shown schematically below:')
+st.write('![texto alternativo](https://pubs.rsc.org/image/article/2017/SC/c7sc02267k/c7sc02267k-f1_hi-res.gif) ')
 st.write('The $$R_i$$ represent the Cartesian coordinates of atom $i$. In a first step these coordinates are transformed into a set of symmetry function values $\{G_i^{\mu}\}$ for each atom $i$. These symmetry function values describe the energetically relevant local environment of each atom and are subsequently used as input for the NN. They depend on the positions of all atoms in the system, as indicated by the dotted arrows.')
 
-st.write('![texto alternativo](https://pubs.rsc.org/image/article/2017/SC/c7sc02267k/c7sc02267k-f1_hi-res.gif) ')
 
-
-
-
-st.write('In the nodes of the input layer the two generalized coordinates $G_i^1$ and $G_i^2$ that determine the energy of configuration $i$ are provided.')
-st.write('The node in the output layer yields the associated energy $E_i$, which in this case depends on the values of the two input nodes $G_i^1$ and $G_i^2$.')
-#st.write('$$f_c(R_{ij})= 0.5\times[\cos(\frac{\pi R_{ij}}{R_c})+1]$$')#  \text{for }R_{ij}\le R_c\\0     \text{for }R_{ij}\gt R_c  $$')
 st.write('$$f_c(R_{ij})=0.5\cos(\pi R_{ij}/R_c)+1$$ for $R_{ij}\le R_c$')
 st.write('and $0$ for $R_{ij}>R_c$:')
 #\times[\cos(\frac{\pi R_{ij}}{R_c})+1]
