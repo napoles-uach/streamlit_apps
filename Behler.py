8683144cafa98148adb65fb4a440fa875d554a19
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-10-22 21:30:52 -0600

    Update Behler.py

--- Behler.py
+++ Behler.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 st.header("Behler")
 
-fig = plt.figure(figsize=(6,8))
+fig = plt.figure(figsize=(4,6))
 Rc=11.0
 x = np.arange(0,11,0.2)
 y = 0.5*(np.cos(np.pi*x/Rc)+1)
