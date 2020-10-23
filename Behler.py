ea29e7dbeca802a28f296e9d035638dffb174f49
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-10-22 21:17:05 -0600

    Update Behler.py

--- Behler.py
+++ Behler.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import numpy as np
-import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+
 
 
 
 st.header("Matplotlib chart in Plotly")
 
-#import matplotlib.pyplot as plt
 
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
-
-#f = plt.figure()
-#arr = np.random.normal(1, 1, size=100)
-#plt.hist(arr, bins=20)
-
-#st.plotly_chart(f)
+st.pyplot(fig)
 
