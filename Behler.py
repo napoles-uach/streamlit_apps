29cea4f94dc92573865d3b6b88bcb3f9dc2573e7
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-10-22 21:26:15 -0600

    Update Behler.py

--- Behler.py
+++ Behler.py
@@ -7,18 +7,15 @@ import matplotlib.pyplot as plt
 
 st.header("Behler")
 
-
-#arr = np.random.normal(1, 1, size=100)
-#fig, ax = plt.subplots()
-#ax.hist(arr, bins=20)
-
-#st.pyplot(fig)
-
-
-Rc=11.0
-x = np.arange(0,11,0.2)
-y = 0.5*(np.cos(np.pi*x/Rc)+1)
-fig, ax =plt.plot(x,y)
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+ax2 = ax1.twinx()
+t = np.linspace(0., 10., 100)
+ax1.plot(t, t ** 2, 'b-')
+ax2.plot(t, 1000 / (t + 1), 'r-')
+ax1.set_ylabel('Density (cgs)', color='red')
+ax2.set_ylabel('Temperature (K)', color='blue')
+ax1.set_xlabel('Time (s)')
 
 st.pyplot(fig)
 
