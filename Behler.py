458314f7a177bebaafbf44cfc406a6c5f6f6dc2c
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-10-22 21:41:26 -0600

    Update Behler.py

--- Behler.py
+++ Behler.py
@@ -6,10 +6,7 @@ import matplotlib.pyplot as plt
 
 
 st.header("Review of Generalized Neural-Network Representation of High-Dimensional Potential-Energy Surfaces")
-st.write('The structure of a simple NN as it has hitherto been used to represent PESs is shown schematically for a 2D PES.
-![texto alternativo](https://github.com/napoles-uach/figuras/blob/master/nn.png?raw=true?raw=true)
-
-In the nodes of the input layer the two generalized coordinates $G_i^1$ and $G_i^2$ that determine the energy of configuration $i$ are provided.
+st.write('The structure of a simple NN as it has hitherto been used to represent PESs is shown schematically for a 2D PES. ![texto alternativo](https://github.com/napoles-uach/figuras/blob/master/nn.png?raw=true?raw=true) In the nodes of the input layer the two generalized coordinates $G_i^1$ and $G_i^2$ that determine the energy of configuration $i$ are provided.
 ')
 
 fig = plt.figure(figsize=(4,2))
