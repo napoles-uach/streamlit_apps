740446f95f8fbe67fba78be14e8a30f7242a0ed6
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-10-24 13:33:25 -0600

    Update Behler.py

--- Behler.py
+++ Behler.py
@@ -79,13 +79,15 @@ y9 = 0.5*(np.cos(np.pi*x/Rc)+1)*np.exp(-3*(x-10)**2)
 
 plt.plot(x,y1,x,y2,x,y3,x,y4,x,y5,x,y6,x,y7,x,y8,x,y9)
 
-#plt.legend(['Rc=11.3'])
+plt.legend(['Rc=11.3'])
 plt.ylim(-.05, 1.05)
 plt.xlabel(r'$R_{ij}$')
 plt.ylabel(r'$G^1$')
 col1, col2 = st.beta_columns((1,1))
 col1.pyplot(fig,use_column_width=True)
 col2.pyplot(fig2,use_column_width=True)
+
+st.write('Radial symmetry functions for an atom with one neighbor only')
 
 
 
