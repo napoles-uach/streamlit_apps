f10434085153ec63c4f9183d8abdda176da2584f
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-10-24 13:34:12 -0600

    Update Behler.py

--- Behler.py
+++ Behler.py
@@ -75,6 +75,7 @@ y5 = 0.5*(np.cos(np.pi*x/Rc)+1)*np.exp(-3*(x-6)**2)
 y6 = 0.5*(np.cos(np.pi*x/Rc)+1)*np.exp(-3*(x-7)**2)
 y7 = 0.5*(np.cos(np.pi*x/Rc)+1)*np.exp(-3*(x-8)**2)
 y8 = 0.5*(np.cos(np.pi*x/Rc)+1)*np.exp(-3*(x-9)**2)
+plt.legend(['Rc=11.3'])
 y9 = 0.5*(np.cos(np.pi*x/Rc)+1)*np.exp(-3*(x-10)**2)
 
 plt.plot(x,y1,x,y2,x,y3,x,y4,x,y5,x,y6,x,y7,x,y8,x,y9)
