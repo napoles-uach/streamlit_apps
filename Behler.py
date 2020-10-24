f56307f98ec574353897ea584b6ce419b819b40a
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-10-24 12:51:06 -0600

    Update Behler.py

--- Behler.py
+++ Behler.py
@@ -22,8 +22,8 @@ indicated by the dotted arrows.')
 st.write('**Radial symmetry functions** are constructed as a sum of Gaussians with the parameters $\eta$ and $R_s$')
 st.latex(r'''G_i^1=\sum_{j\ne i}^{all} e^{-\eta (R_{ij}-R_s)^2}f_c(R_{ij})''')
 
-st.latex('''f_c(R_{ij})=0.5\cos(\pi R_{ij}/R_c)+1 for $R_{ij}\le R_c$')
-#st.latex('''f_c(R_{ij})= 0.5\times[\cos(\frac{\pi R_{ij}}{R_c})+1]  \text{for }R_{ij}\le R_c''')
+st.latex('''f_c(R_{ij})=0.5\cos(\pi R_{ij}/R_c)+1 \text{for } R_{ij}\le R_c''')
+# st.latex('''f_c(R_{ij})= 0.5\times[\cos(\frac{\pi R_{ij}}{R_c})+1]  \text{for }R_{ij}\le R_c''')
 
 st.write('and $0$ for $R_{ij}>R_c$:')
 
